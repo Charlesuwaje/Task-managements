2badff37a73dd451884a7481bc8ece08ecd28067
Charles Uwaje <charlesuwaje02@gmail.com>
2025-07-03 02:12:47 +0100

    push

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -1,4 +1,4 @@
-<!-- <?php
+<?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -31,4 +31,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('personal_access_tokens');
+    }
     
+
+};
